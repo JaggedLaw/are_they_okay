2a2cf2acbe60e9a590812e738648a58f85003a90
ZooBot Mars <sasa345@yahoo.com>
2016-04-23 16:10:44 -0600

    remove survey reference from questions

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,5 +2,5 @@ class Question < ActiveRecord::Base
   belongs_to :survey
   has_many :illness_questions
   has_many :illnesses, through: :illness_questions
-  
+
 end
--- db/migrate/20160421225444_create_questions.rb
+++ db/migrate/20160421225444_create_questions.rb
@@ -1,7 +1,6 @@
 class CreateQuestions < ActiveRecord::Migration
   def change
     create_table :questions do |t|
-      t.references :survey, index: true, foreign_key: true
       t.string :question
 
       t.timestamps null: false
